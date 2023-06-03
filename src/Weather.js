94f73b514824db6118c552aee850da0daf8f4328
QueenB-31 <125220221+Queency31NSPPD@users.noreply.github.com>
2023-06-03 17:06:03 +0100

    Created WeatherInfo.js file

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 
  import React, { useState } from 'react';
- import FormattedDate from './FormattedDate';
+ 
+ import WeatherInfo from './WeatherInfo';
  import './Weather.css';
  import axios from 'axios';
  
@@ -8,7 +9,8 @@
  export default function Weather(props) {
  
      const [weatherData, setWeatherData] = useState({ ready: false });
- 
+ const [city, setCity] = useState(props.defaultCity)
+
      function handleResponse(response) {
          console.log(response.data);
          setWeatherData({
@@ -23,17 +25,35 @@
          });
  
      }
+
+function search(){
+     let apiKey = '0b8bet4102f106df6eef01d97o5b3bab';
+         let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+         axios.get(apiUrl).then(handleResponse);
  
+}
+
+     function handleSubmit(event){
+        event.preventDefault();
+    search(city); 
+    }
+     
+    function handleCityChange(event){
+setCity(event.target.value);
+    }
+
      if (weatherData.ready) {
          return (
              <div className="Weather">
-                 <form>
+                 <form onSubmit={handleSubmit}>
+                   
                      <div className='row'>
                          <div className='col-9'>
                              <input
                                  type='search' placeholder='Enter a city name...'
                                  className='form-control'
                                  autoFocus='on'
+                                 onChange={handleCityChange}
                              />
                          </div>
                          <div className='col-3'>
@@ -42,48 +62,12 @@
                          </div>
                      </div>
                  </form>
-                 <h1>{weatherData.city}</h1>
-                 <ul>
-                     <li className='text-capitalize'>
-                         <FormattedDate date={weatherData.date} />
- 
-                     </li>
-                     <li className='text-capitalize'>
-                         {weatherData.description}
-                     </li>
-                 </ul>
-                 <div className='row mt-3'>
-                     <div className='col-6'>
-                         <div className='clearfix'>
-                             <img src={weatherData.icon_url}
-                                 alt={weatherData.description}
-                                 className='float-left'
-                             />
- 
-                             <span className='temperature'>{Math.round(weatherData.temperature)}</span>
-                             <span className='unit'>°C</span>
-                         </div>
-                     </div>
- 
- 
-                     <div className='col-6'>
-                         <ul>
-                             <li>
-                                 Humidity:{weatherData.humidity}%
-                             </li>
-                             <li>
-                                 Wind:{Math.round(weatherData.wind)}km/h
-                             </li>
-                         </ul>
-                     </div>
-                 </div>
-             </div>
-         );
+                  <WeatherInfo info={weatherData} />
+                  </div>
+                );
      } else {
-         let apiKey = '0b8bet4102f106df6eef01d97o5b3bab';
-         let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
-         axios.get(apiUrl).then(handleResponse);
- 
+        search();
+
          return 'loading...';
  
      } 
---
+++ src/WeatherInfo.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import FormattedDate from './FormattedDate';
+
+export default function WeatherInfo(props) {
+    return (
+        <div className='WeatherInfo'> 
+     <h1>{props.info.city}</h1>
+                 <ul>
+                     <li className='text-capitalize'>
+                         <FormattedDate date={props.info.date} />
+ 
+                     </li>
+                     <li className='text-capitalize'>
+                         {props.info.description}
+                     </li>
+                 </ul>
+                 <div className='row mt-3'>
+                     <div className='col-6'>
+                         <div className='clearfix'>
+                             <img src={props.info.icon_url}
+                                 alt={props.info.description}
+                                 className='float-left'
+                             />
+ 
+                             <span className='temperature'>{Math.round(props.info.temperature)}</span>
+                             <span className='unit'>°C</span>
+                         </div>
+                     </div>
+ 
+ 
+                     <div className='col-6'>
+                         <ul>
+                             <li>
+                                 Humidity:{props.info.humidity}%
+                             </li>
+                             <li>
+                                 Wind:{Math.round(props.info.wind)}km/h
+                             </li>
+                         </ul>
+                     </div>
+                 </div>
+             </div>
+         );
+}
